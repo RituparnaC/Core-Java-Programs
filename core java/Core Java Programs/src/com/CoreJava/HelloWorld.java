1ed63df70a33d047bf488d8fab507e7d9759aa94
Rituparna Chowdhury <43441491+RituparnaC@users.noreply.github.com>
2019-04-24 16:32:01 +0530

    Add files via upload

---
+++ core java/Core Java Programs/src/com/CoreJava/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.CoreJava;
+
+public class Program1{
+
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Learning Core Java For Selenium");
+
+		}
+
+	}
+
+
---
+++ core java/MyWorkspace/CoreJava/src/com/java/ifElse.java
@@ -0,0 +1,32 @@
+package com.java;
+
+public class ifElse {
+
+	public static void main(String[] args) {
+		
+		int n = 1;
+		if (n % 2 == 0)
+		{
+		
+			if(n >= 2 && n <= 5) 
+			{
+				
+				System.out.println("not weird"); 
+			}
+			else if(n >= 6 && n <= 20) 
+			{
+				System.out.println("Weird");
+		    }
+		
+			else if (n > 20) {
+				System.out.println("Not Weird");
+			}
+				
+		}
+		else 
+		{
+				System.out.println("weird");
+		}
+	}
+
+}
